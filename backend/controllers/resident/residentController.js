4c7e3ad43827c74366c77bf21b6e5b2222e969ec
moeezali <moeezali2375@gmail.com>
2023-10-21 09:57:10 +0500

    add multer, images model

--- backend/controllers/resident/residentController.js
+++ backend/controllers/resident/residentController.js
@@ -1,4 +1,42 @@
+const Bill = require("../../models/bill");
+const Image = require("../../models/image");
+const upload = require("../../middlewares/multer");
 
 module.exports.home = (req, res) => {
 	res.send("Home page of Resident!");
+};
+
+module.exports.get_bills = async (req, res) => {
+	try {
+		const bills = await Bill.find({
+			userid: req.user._id,
+		});
+		res.status(200).json(bills);
+	} catch (error) {
+		res.status(400).send(error.message);
+	}
+};
+
+module.exports.payment_details = async (req, res) => {
+	upload(req, res, (err) => {
+		if (err) {
+			res.status(400).send(err.message);
+		} else {
+			const newImage = new Image({
+				name: req.body.name,
+				image: {
+					data: req.file.filename,
+					contentType: "image/png",
+				},
+			});
+			newImage
+				.save()
+				.then(() => {
+					res.status(200).send("Picture Uploaded Successfully!");
+				})
+				.catch((err) => {
+					res.status(400).send(err.message);
+				});
+		}
+	});
 };
--- backend/routes/resident/residentRoutes.js
+++ backend/routes/resident/residentRoutes.js
@@ -1,9 +1,18 @@
 const express = require("express");
+
 const { residentAuth } = require("../../middlewares/residentAuth");
-const { home } = require("../../controllers/resident/residentController");
+const {
+	home,
+	get_bills,
+	payment_details,
+} = require("../../controllers/resident/residentController");
 
 const residentRouter = express.Router();
 
 residentRouter.get("/home", residentAuth, home);
+
+residentRouter.get("/bills", residentAuth, get_bills);
+
+residentRouter.post("/bills/payment-details", residentAuth, payment_details);
 
 module.exports = residentRouter;
